526735095469230507dac1873eb352c3f4a15e2e
sh1nez <biv888iii888@yandex.ru>
2024-07-25 01:34:47 +0300

    errors

---
+++ lib/errors.asm
@@ -0,0 +1,29 @@
+global invalid_syntaxsys
+global division_by_zero
+
+division_by_zero_error:
+	mov rax, 1
+	mov rdi, 1
+    mov rsi, division_by_zero_message
+    mov rdx, division_by_zero_length
+	syscall
+    ret
+
+invalid_syntaxsys_error:
+	mov rax, 1
+	mov rdi, 1
+    mov rsi, invalid_syntaxsys_message
+    mov rdx, invalid_syntaxsys_length
+	syscall
+
+	mov rax, 60
+	xor rdi, rdi
+    ret
+
+section .data
+    division_by_zero_message db "division by zero", 0xA
+    division_by_zero_length equ $ - division_by_zero_message
+    invalid_syntaxsys_message db "invalid syntaxsys", 0xA
+    invalid_syntaxsys_length equ $ - invalid_syntaxsys_message
+
+
